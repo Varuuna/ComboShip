6dae36293269ec0f322a2a5d9418cf7adb6a6750
Fredrik Andréasson <medumee@gmail.com>
2025-09-30 18:54:55 +0200

    Building

---
+++ soh/src/boot/build.c
@@ -0,0 +1,14 @@
+#include <libultraship/libultra.h>
+
+const char gBuildVersion[] = "Combo Build (1.0.0)";
+const u16 gBuildVersionMajor = 1;
+const u16 gBuildVersionMinor = 0;
+const u16 gBuildVersionPatch = 0;
+
+const char gGitBranch[] = "master";
+const char gGitCommitHash[] = "b2bd37f";
+const char gGitCommitTag[] = "";
+
+const char gBuildTeam[] = "github.com/harbourmasters";
+const char gBuildDate[] = __DATE__ " " __TIME__;
+const char gBuildMakeOption[] = "";
